8ecbd6f86f9290251ed560d6159dcd5c51c9fdc2
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2024-10-01 19:30:16 +0600

    Add Tailwind CSS configuration and styles

--- src/app.rs
+++ src/app.rs
@@ -4,9 +4,9 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+           
+            <h1 class="underline">{ "Hello World!" }</h1>
+            <p>{ "Welcome to the Yew App! test" }</p>
         </main>
     }
 }
